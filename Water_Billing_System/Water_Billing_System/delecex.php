2ba99675272370fff45e251f5c7e78166a2544ea
singuyenvan0206 <96609459+singuyenvan0206@users.noreply.github.com>
2024-06-12 08:01:22 +0700

    Update delecex.php

--- Water_Billing_System/Water_Billing_System/delecex.php
+++ Water_Billing_System/Water_Billing_System/delecex.php
@@ -1,8 +1,27 @@
 <?php
 include 'db.php';
 	$id = $_POST['id'];
-	mysqli_query($conn,"DELETE from owners WHERE id='$id'");
-			
+	$deletes_query = "DELETE FROM owners WHERE id = $id";
+	if (mysqli_query($conn, $deletes_query)) {
+		// Success message
+		echo '<script>alert("Client deleted successfully")</script>';
+		
+		// Fetch all users
+		$clients_result = mysqli_query($conn, "SELECT * FROM owners");
+		
+		// Counter for sequential IDs
+		$counter = 1;
+		
+		// Update user records with sequential IDs
+		while ($clients = mysqli_fetch_assoc($clients_result)) {
+			$update_query = "UPDATE owners SET id = $counter WHERE id = {$clients['id']}";
+			mysqli_query($conn, $update_query);
+			$counter++;
+		}
+	} else {
+		// Error message
+		echo '<script>alert("Error: ' . mysqli_error($conn) . '")</script>';
+	}
 
 		 echo "<script>windows: location='clients.php'</script>";				
 			
